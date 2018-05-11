b80befbcbe6add9a3cc23b2a601acbf48f1e3034
pabloaredu <pablo.adu83@gmail.com>
2018-05-11 02:24:54 +0000

    Created main templates

---
+++ express_server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
+
+
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    urls: urlDatabase
+  };
+  res.render("urls_show", templateVars);
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+
